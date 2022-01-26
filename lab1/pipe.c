fb842f0c6fdc8d9e0e0314f016b758a6df617495
Matthew Yu <yumatthew10@gmail.com>
2022-01-25 23:40:33 -0800

    Add pipe functionality for 3 commands

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -41,21 +41,25 @@ int main(int argc, char *argv[])
 
         if (pid == 0) //child process
         {
-            if (dup2(fd[1], STDOUT_FILENO) == -1){
+            if (dup2(fd[1], STDOUT_FILENO) == -1)
+            {
                 return errno;
             }
-            if (close(fd[1]) == -1 || close(fd[0]) == -1){
+            if (close(fd[1]) == -1 || close(fd[0]) == -1)
+            {
                 return errno;
             }
 
-            if (execlp(argv[1], argv[1], (char *)NULL) == -1){
+            if (execlp(argv[1], argv[1], (char *)NULL) == -1)
+            {
                 return errno;
             }
         }
         else if (pid > 0) //parent process
         {
             close(fd[1]);
-            if (wait(&wstatus) == -1){
+            if (wait(&wstatus) == -1)
+            {
                 return errno;
             }
         }
@@ -63,7 +67,7 @@ int main(int argc, char *argv[])
         {
             return errno;
         }
-
+        //2nd commnad
         pid = fork();
         if (pid == 0) //child process
         {
@@ -85,5 +89,96 @@ int main(int argc, char *argv[])
     }
     else if (argc == 4)
     {
+        int fd[2][2]; // fd[0] for read, fd[1] for write
+        pipe(fd[0]);
+
+        int wstatus;
+        int pid = fork();
+
+        if (pid == 0) //child process
+        {
+            if (dup2(fd[0][1], STDOUT_FILENO) == -1)
+            {
+                return errno;
+            }
+            if (close(fd[0][1]) == -1 || close(fd[0][0]) == -1)
+            {
+                return errno;
+            }
+
+            if (execlp(argv[1], argv[1], (char *)NULL) == -1)
+            {
+                return errno;
+            }
+        }
+        else if (pid > 0) //parent process
+        {
+            close(fd[0][1]);
+            if (wait(&wstatus) == -1)
+            {
+                return errno;
+            }
+        }
+        else
+        {
+            return errno;
+        }
+        //2nd command
+        
+        pipe(fd[1]);
+        pid = fork();
+        if (pid == 0) //child process
+        {
+            if (dup2(fd[0][0], STDIN_FILENO) == -1 || dup2(fd[1][1], STDOUT_FILENO) == -1)
+            {
+                return errno;
+            }
+
+            if (close(fd[0][0]) == -1 || close(fd[0][1]) == -1 || close(fd[1][1]) == -1)
+            {
+                return errno;
+            }
+
+            if (execlp(argv[2], argv[2], (char *)NULL) == -1)
+            {
+                return errno;
+            }
+        }
+        else if (pid > 0) //parent process
+        {
+            close(fd[0][0]);
+            close(fd[1][1]);
+            wait(&wstatus);
+        }
+        else
+        {
+            return errno;
+        }
+
+        //3rd command
+        pid = fork();
+        if (pid == 0){
+            if (dup2(fd[1][0], STDIN_FILENO) == -1){
+                return errno;
+            }
+            if (close(fd[1][0]) == -1){
+                return errno;
+            }
+            if (execlp(argv[3], argv[3], (char *)NULL) == -1){
+                return errno;
+            }
+
+        }
+        else if (pid > 0) //parent process
+        {
+            close(fd[1][0]);
+            wait(&wstatus);
+        }
+        else
+        {
+            return errno;
+        }
+    
+
     }
 }
